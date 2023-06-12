ddaa57ec6745e3b2b4dcf91e7c0ee7d4d63a282d
Nathan Farquhar-Kay <nathanfarquhar-kay@Nathans-Mac-mini.local>
2023-06-12 11:04:28 -0400

    project layout complete

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-// Assignment code here
+// **write var arrays to establish uppercase,lowercase,numbers and special char.
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+
 // Write password to the #password input
 function writePassword() {
+  //** true or false statement to ensure you have the right promps 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -15,3 +17,6 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// ** write function for true/false statement to ask if they want uppercase,lowercase,numbers and special char.
+// **use if statments and set an alert if no number is selected when they select password length (8-128)
