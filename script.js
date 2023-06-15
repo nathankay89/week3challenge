a100993ab775399de9550750ece99fbbb0a7d535
Nathan Farquhar-Kay <nathanfarquhar-kay@Nathans-Mac-mini.local>
2023-06-15 09:09:41 -0400

    Array,T/F function and additional choices added

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-// **write var arrays to establish uppercase,lowercase,numbers and special char.
+// Add var arrays to include uppercase/lowercase/numbers/special characters.
+
+var uppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+var lowercase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','z']
+var num = ['1','2','3','4','5','6','7','8','9','0']
+var spec = ['!','@','#','$','%','^','&','*','<','-','+']
+
+// Add charLength and choice array
+
+var characterLength = 8;
+var choiceArr = [];
 
 
 // Get references to the #generate element
@@ -18,5 +28,29 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// ** write function for true/false statement to ask if they want uppercase,lowercase,numbers and special char.
-// **use if statments and set an alert if no number is selected when they select password length (8-128)
+function generatePassword (){
+
+}
+// function to promp and return "try again" if response is anything but a number or 8-128 char.
+function getPrompts(){
+    characterLength = prompt ("How many characters do you want your password to be? (choose between 8-128 characters)")
+}
+if (isNaN(characterLength) || characterLength > 128 || characterLength< 8){
+    alert("Character length has to be a number between 8 to 128. Try again");
+    return false;
+  }
+// additional promps to include uppercase/lowercase/numbers/special characters.
+
+ if (confirm ('Would you like uppercase letters in your password?')){
+    choiceArr = choiceArr.concat(uppercase);
+ }
+if (confirm ('Would you like lowercase letters in your password?')){
+    choiceArr = choiceArr.concat(lowercase);
+}
+if (confirm ('Would you like numbers in your password?')){
+    choiceArr = choiceArr.concat(num);
+}
+if (confirm ('Would you like special characters in your password?')){
+    choiceArr = choiceArr.concat(spec);
+}
+return true;
